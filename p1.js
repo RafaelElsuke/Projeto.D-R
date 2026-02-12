c432bd6d86c584a038e352590a9e62efa1643e99
Rafael rodrigues <120226602+RafaelElsuke@users.noreply.github.com>
2026-02-12 12:55:57 -0300

    Add files via upload

--- p1.js
+++ p1.js
@@ -1,15 +1,23 @@
-const carousel= document.getElementById('carousel');
-let scrollAmont=0;
-const scrollStep=2;
-const delay= 30;
+const carousel = document.getElementById('carousel');
 
-function autoScroll(){
+let scrollAmount = 0;
+const scrollStep = 1;
+const delay = 10;
+
+function autoScroll() {
     carousel.scrollLeft += scrollStep;
-    if(carousel.scrollLeft >= (carousel.scrollWidth - carousel.clientWidth)){
+
+    if (carousel.scrollLeft >= carousel.scrollWidth - carousel.clientWidth) {
         carousel.scrollLeft = 0;
     }
-    
-} 
-let time = setInterval(autoScroll, delay);
-carousel.addEventListener('mouseenter', () =>clearInterval(timer));
-carousel.addEventListener('mouseleave',()=>clearInterval(autoScroll,delay));
+}
+
+let timer = setInterval(autoScroll, delay);
+
+carousel.addEventListener('mouseenter', () => {
+    clearInterval(timer);
+});
+
+carousel.addEventListener('mouseleave', () => {
+    timer = setInterval(autoScroll, delay);
+});
