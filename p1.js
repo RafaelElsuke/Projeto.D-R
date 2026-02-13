14f0c55ee3c41bb4ce79be518c1a57adab07be72
HOME <you@example.com>
2026-02-13 12:30:55 -0300

    Resolvendo

--- p1.js
+++ p1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const carousel = document.getElementById('carousel');
 
 let scrollAmount = 0;
@@ -22,28 +21,3 @@ carousel.addEventListener('mouseenter', () => {
 carousel.addEventListener('mouseleave', () => {
     timer = setInterval(autoScroll, delay);
 });
-=======
-const carousel = document.getElementById('carousel');
-
-let scrollAmount = 0;
-const scrollStep = 1;
-const delay = 10;
-
-function autoScroll() {
-    carousel.scrollLeft += scrollStep;
-
-    if (carousel.scrollLeft >= carousel.scrollWidth - carousel.clientWidth) {
-        carousel.scrollLeft = 0;
-    }
-}
-
-let timer = setInterval(autoScroll, delay);
-
-carousel.addEventListener('mouseenter', () => {
-    clearInterval(timer);
-});
-
-carousel.addEventListener('mouseleave', () => {
-    timer = setInterval(autoScroll, delay);
-});
->>>>>>> b874cfdf56aa33879a4de9ed03959f18164fff0c
